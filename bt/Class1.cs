24d8fae216c02c5e57d6c6c79cb8fbda156d3625
chinh <anhvohoanghuy25@gmail.com>
2024-01-19 11:03:15 +0700

    Add project files.

---
+++ bt/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt
+{
+    public class Sanpham
+    {
+        public string MaSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public int DonGia { get; set; }
+        public int SoLuong { get; set; }
+
+        public Sanpham()
+        {
+
+        }
+        public Sanpham(string MaSanPham, string TenSanPham, int DonGIa, int SoLuong)
+        {
+            this.MaSanPham = MaSanPham;
+            this.TenSanPham = TenSanPham;
+            this.DonGia = DonGIa;
+            this.SoLuong = SoLuong;
+        }
+        public int TinhTien()
+        {
+            return this.DonGia * this.SoLuong;
+        }
+        public void Information()
+        {
+            Console.WriteLine("Ma san pham: {0}", this.MaSanPham);
+            Console.WriteLine("Ten san pham: {0}", this.TenSanPham);
+            Console.WriteLine("Don gia: {0}", this.DonGia);
+            Console.WriteLine("So luong: {0}", this.SoLuong);
+            Console.WriteLine("Thanh tien: {0}", TinhTien());
+        }
+    }
+}
---
+++ bt/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap so san pham: ");
+            int n = int.Parse(Console.ReadLine());
+            ArrayList sp=new ArrayList();
+            Sanpham[] sparray = new Sanpham[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhap ma SP: ");
+                string MaSP= Console.ReadLine();
+                Console.WriteLine("Nhap ten SP: ");
+                string TenSP = Console.ReadLine();
+                Console.WriteLine("Nhap gia SP:");
+                int GiaSP = int.Parse(Console.ReadLine());
+                Console.WriteLine("Nhap so luong: ");
+                int SLSP= int.Parse(Console.ReadLine());
+                Sanpham sanpham = new Sanpham(MaSP,TenSP,GiaSP,SLSP);
+                sparray[i] = sanpham;
+                sp.Add(sanpham);
+            }
+            foreach (Sanpham item in sp)
+            {
+                item.Information();
+            }
+            for (int i = 0;i < n;i++)
+                sparray[i].Information();
+
+            Console.ReadKey();
+        }
+    }
+}
